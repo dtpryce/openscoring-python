c0a4b478867fa2ba56ac615b5f6d7764cd4ad5c1
David Pryce <david.pryce@kpmg.co.uk>
2015-11-11 11:22:17 +0000

    Change of name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-	name = "openscoring-dev",
+	name = "openscoring_dev",
 	version = "0.1.1",
 	description = "Python client library for the Openscoring REST web service (https://github.com/jpmml/openscoring)",
 	author = "David Pryce",
 	author_email = "dtpryce1988@gmail.com",
 	url = "https://github.com/jpmml/openscoring-python",
 	license = "GNU Affero General Public License (AGPL) version 3.0",
-	packages = ["openscoring-dev"],
+	packages = ["openscoring_dev"],
 	install_requires = [
 		"requests"
 	]
